888a0e53fb19c00c0ff1aef0e9475708643c0843
dyr4 <118971998+dyr4@users.noreply.github.com>
2024-12-28 21:03:19 +0500

    Delete src directory

--- src/scripts/contact.js
+++
@@ -1,9 +0,0 @@
-function checkPhone() {
-    var phone = document.getElementById("phone").value.trim();
-    var regex = /^\+7\(\d{3}\)-\d{3}-\d{2}-\d{2}$/;
-  
-    return regex.test(phone);
-  }
-  
-  export default checkPhone;
-  
